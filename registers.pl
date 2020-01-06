ef488a70a02e0535d3e0fca4354832c9e3889bca
Sumit Srivastava <srv.sumit@gmail.com>
2020-01-06 17:22:37 +0100

    Swap method added in registers

--- registers.pl
+++ registers.pl
@@ -32,3 +32,35 @@ pc([_|T], X, Y, Y, [SV|Res], SV):-
     NL is Y + 1,
     pc(T, X, Y, NL, Res, SV).
 
+
+%Perform swap of two values in a list.
+perform_swap(List, X, Y, Res):-
+    ps(List, X, Y, 1, 1, _, _, Res).
+
+ps([], _, _, _, _, _, _, []).
+
+%Neither source, nor destination
+ps([H|T], X, Y, A, B, E1, E2, [H|Res]):-
+    X\=A,
+    Y\=B,
+    AN is A +1,
+    BN is B+1,
+    ps(T, X, Y, AN, BN, E1, E2, Res).
+
+%Source
+ps([H|T], X, Y, X, B, H, E2, [E2|Res]):-
+    Y\=B,
+    AN is X+1,
+    BN is B+1,
+    ps(T, X, Y, AN, BN, H, E2, Res).
+
+%Destination
+ps([H|T], X, Y, A, Y, E1, H, [E1|Res]):-
+    A\=X,
+    AN is A+1,
+    BN is Y+1,
+    ps(T, X, Y, AN, BN, E1, H, Res).
+
+
+
+
